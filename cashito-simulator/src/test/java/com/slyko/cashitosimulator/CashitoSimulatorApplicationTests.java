2447430263abf8ff5e4cc47422161bb1da6ff9ff
Oskar Slyk <oskar.slyk@vodeno.com>
2024-09-07 10:01:38 +0200

    simulator

---
+++ cashito-simulator/src/test/java/com/slyko/cashitosimulator/CashitoSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slyko.cashitosimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashitoSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
