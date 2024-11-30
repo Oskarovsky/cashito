fc83b57eaf51a0bae9a7f7de33c18b8a61d2eb72
Oskar Slyk <oskar.slyk@vodeno.com>
2024-11-30 21:52:14 +0100

    simualtor;

---
+++ cashito-simulator/src/main/java/com/slyko/cashitosimulator/CashitoSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.slyko.cashitosimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashitoSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashitoSimulatorApplication.class, args);
+    }
+
+}
